2df8fba2c0a79c3b5b258325cc3f57e871cfd231
Jack <peppyjack2000@gmail.com>
2020-01-28 15:24:20 -0500

    Added JS to change large button text content  to Learn More

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,7 +1,7 @@
 /*
-    Student Name  :
-    StudentID     :
-    Date Completed:
+    Student Name  : Jack Ingram
+    StudentID     : 100710241
+    Date Completed: Jan 28th 2019
 */
 
 "use strict";
@@ -31,6 +31,18 @@ let app;
      */
     function Main()
     {
+        //change text content from "Large Button" to "Learn More"
+        let buttonElement = document.getElementById("largeButton");
+        buttonElement.textContent = "Learn More";
+
+        //add personal student info (name and student id to a new h3 element and insert it before the large button)
+        let h3Element = document.createElement("h3");
+        let mainDivContent = document.getElementById("mainContent");
+        h3Element.textContent = "[Jack Ingram] [100710241]";
+        .insertBefore(h3Element, buttonElement);
+        console.log(buttonElement);
+
+
 
     }
 
